3e725a339882cc4a66e7acdeba882d929793f661
Dadoudidou <davidviolet18@gmail.com>
2019-05-17 15:03:00 +0200

    GraphQL Functions

---
+++ src/GraphQL/Utils/ToObjectType.ts
@@ -0,0 +1,22 @@
+
+export function toObjectType<T>(Type: new (...args: any[]) => T, object: any) : T {
+    let _newClass: any = new Type();
+    let _obj = object;
+
+    if(_obj["toJSON"]) _obj = _obj["toJSON"]();
+    for(let key in _obj){
+        _newClass[key] = _obj[key];
+    }
+
+    return _newClass;
+}
+
+export function toObjectTypeArray<T>(Type: new (...args: any[]) => T, objects: any[]) : T[] {
+
+    let _array: T[] = [];
+    if(!objects) return _array;
+    for(let i=0; i<objects.length; i++){
+        _array.push(toObjectType<T>(Type, objects[i]));
+    }
+    return _array;
+}
